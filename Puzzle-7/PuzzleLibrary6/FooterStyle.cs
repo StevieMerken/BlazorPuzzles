1ab76bb026d1dabf301b0b6b20a7a5528d0fe4a0
Stevie Merken <stevie.merken@gmail.com>
2024-03-15 08:28:34 +0100

    Solution puzzle 7

--- Puzzle-7/PuzzleLibrary6/FooterStyle.cs
+++ Puzzle-7/PuzzleLibrary6/FooterStyle.cs
@@ -1,10 +1,26 @@
 namespace PuzzleLibrary6;
 
+internal class FooterStyle
+{
+	private bool isDarkMode;
 
-	internal class FooterStyle {
+	public string FooterClass { get; set; } = string.Empty;
 
-		public string FooterClass { get; set; } = string.Empty;
+	public int NumColumns { get; set; } = 1;
 
-		public int NumColumns { get; set; } = 1;
+	public bool IsDarkMode 
+	{ 
+		get => isDarkMode; 
+		set 
+		{ 
+			if(isDarkMode == value) 
+				return;
 
+			isDarkMode = value; 
+			OnDarkModeChanged?.Invoke(this, EventArgs.Empty);
+		}
 	}
+
+	public event EventHandler? OnDarkModeChanged;
+
+}
